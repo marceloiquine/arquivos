dbf99f1d4ffc92451e8c6d83370002445eba2e99
marcelo0211 <marcelolira@outlook.com>
2023-01-23 19:53:12 -0300

    adicionar o arquivo app.py

---
+++ app1.py
@@ -0,0 +1,7 @@
+pessoas=['maria','fernado','marilia','manoel']
+idade = [21,23,43,22]
+
+for i,v in enumerate(pessoas):
+    id = idade[i]
+    pessoa = pessoas[i]
+    print('{} tem {} anos'.format(pessoa, id))
