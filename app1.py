04d5e418668e6685f03b6d9afed7a86817928b34
marceloiquine <124437921+marceloiquine@users.noreply.github.com>
2023-02-04 08:37:21 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
-pessoas=['maria','fernado','marilia','manoel']
-idade = [21,23,43,22]
+pessoas=['maria','fernado','marilia','manoel','karina']
+idade = [21,23,43,22,55]
 
 for i,v in enumerate(pessoas):
     id = idade[i]
